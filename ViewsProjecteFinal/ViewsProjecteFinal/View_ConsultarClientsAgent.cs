5c0864a0f33cb0f34d5d0c784e5d30d80e3ea1a7
aleixventura <aleixventura@hotmail.com>
2015-05-21 17:12:50 +0200

    avp

--- ViewsProjecteFinal/ViewsProjecteFinal/View_ConsultarClientsAgent.cs
+++ ViewsProjecteFinal/ViewsProjecteFinal/View_ConsultarClientsAgent.cs
@@ -7,15 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ViewsProjecteFinal.CustomClasses;
 
 namespace ViewsProjecteFinal
 {
     public partial class View_ConsultarClientsAgent : Form
     {
-        public View_ConsultarClientsAgent()
+        PersistanceManager pm;
+        public View_ConsultarClientsAgent(String id)
         {
             InitializeComponent();
             this.lblTitolView.ForeColor = Color.FromArgb(26, 183, 234);
+            pm = new PersistanceManager();
+            this.gridView.DataSource = pm.ClientsdelComercial(pm.getComercial(int.Parse(id))).ToList();
         }
+
     }
 }
