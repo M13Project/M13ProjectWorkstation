36b7d19c4d6b0ad2f8566b1fae33eada03eda400
markk15 <mmaark15@gmail.com>
2015-05-23 14:51:40 +0200

    mnbj

--- ViewsProjecteFinal/ViewsProjecteFinal/E_AgentMesClients_View.Designer.cs
+++ ViewsProjecteFinal/ViewsProjecteFinal/E_AgentMesClients_View.Designer.cs
@@ -28,20 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.lblTitolView = new System.Windows.Forms.Label();
+            this.chartAgentsMesClients = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartAgentsMesClients)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblTitolView
+            // 
+            this.lblTitolView.AutoSize = true;
+            this.lblTitolView.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitolView.Font = new System.Drawing.Font("Palatino Linotype", 40F);
+            this.lblTitolView.Location = new System.Drawing.Point(379, 29);
+            this.lblTitolView.Name = "lblTitolView";
+            this.lblTitolView.Size = new System.Drawing.Size(604, 73);
+            this.lblTitolView.TabIndex = 17;
+            this.lblTitolView.Text = "Agent with most clients";
+            // 
+            // chartAgentsMesClients
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartAgentsMesClients.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartAgentsMesClients.Legends.Add(legend1);
+            this.chartAgentsMesClients.Location = new System.Drawing.Point(277, 118);
+            this.chartAgentsMesClients.Name = "chartAgentsMesClients";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartAgentsMesClients.Series.Add(series1);
+            this.chartAgentsMesClients.Size = new System.Drawing.Size(776, 416);
+            this.chartAgentsMesClients.TabIndex = 18;
+            this.chartAgentsMesClients.Text = "chartAgentsMesClients";
             // 
             // E_AgentMesClients_View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ViewsProjecteFinal.Properties.Resources.fonsViews;
             this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.Controls.Add(this.chartAgentsMesClients);
+            this.Controls.Add(this.lblTitolView);
             this.Name = "E_AgentMesClients_View";
             this.Text = "E_AgentMesClients_View";
             this.Load += new System.EventHandler(this.E_AgentMesClients_View_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartAgentsMesClients)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitolView;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartAgentsMesClients;
+
     }
 }
--- ViewsProjecteFinal/ViewsProjecteFinal/E_AgentMesClients_View.cs
+++ ViewsProjecteFinal/ViewsProjecteFinal/E_AgentMesClients_View.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ViewsProjecteFinal.ServiceReference;
+using ViewsProjecteFinal.CustomClasses;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace ViewsProjecteFinal
 {
     public partial class E_AgentMesClients_View : Form
     {
+        m13_projectEntities1 remoteDataContext;
+        PersistanceManager pm;
         public E_AgentMesClients_View()
         {
             InitializeComponent();
@@ -21,6 +26,68 @@ namespace ViewsProjecteFinal
         private void E_AgentMesClients_View_Load(object sender, EventArgs e)
         {
             this.WindowState = FormWindowState.Maximized;
+            provaChart();
+        }
+        public void provaChart()
+        {
+            pm = new PersistanceManager();
+            remoteDataContext = new m13_projectEntities1(new Uri("http://localhost:52220/M13ProjectWcfDataService.svc/"));
+            //var usuaris = localEntities.Usuari.Include(g => g.);
+            //Usuari us = (from user in remoteDataContext.Usuari select user);
+            //var query = remoteDataContext.LoadProperty(us, "Comercial");
+
+            chartAgentsMesClients.Series["Nom"].ChartType = SeriesChartType.Bar;
+            chartAgentsMesClients.Series["Nom"].Points.AddY(1);
+            chartAgentsMesClients.Series["Nom"].ChartArea = "ChartArea1";
+            //Add a chart at run time.
+            chartAgentsMesClients.Series.Add("RuntimeSeries");
+            chartAgentsMesClients.Series["RuntimeSeries"].ChartType = SeriesChartType.Bar;
+            chartAgentsMesClients.Series["RuntimeSeries"].ChartArea = "ChartArea2";
+            //foreach (var usuari in query)
+            {
+                //chartAgentsMesClients.Series["RuntimeSeries"].Points.AddY(usuari.Comercial.Client.Count);
+            }    
         }
     }
 }
+//// TODO: This line of code loads data into the 'MyTestDatabaseDataSet.Usuari' table. You can move, or remove it, as needed.
+//            this.UsuariTableAdapter.Fill(this.MyTestDatabaseDataSet.Usuari);
+//            //label1.Text = ComputeHash("qwertyuiop", new SHA256CryptoServiceProvider());
+//            this.reportViewer1.RefreshReport();
+
+//            provaInforme();
+//        }
+
+//        //public static string ComputeHash(string input, HashAlgorithm algorithm)
+//        //{
+//        //    Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+//        //    Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+
+//        //    String hashString = BitConverter.ToString(hashedBytes);
+
+//        //    return hashString;
+//        //}
+
+//        public void provaInforme() {
+//            MyTestDatabaseEntities localEntities = new MyTestDatabaseEntities();
+
+//            //var usuaris = localEntities.Usuari.Include(g => g.);
+
+//            var eager = localEntities.Usuari.Include("Comercial.Client");     
+            
+
+//            chart1.Series["Edat"].ChartType = SeriesChartType.Bar;
+//            chart1.Series["Edat"].Points.AddY(1);
+//            chart1.Series["Edat"].ChartArea = "ChartArea1";
+
+//            //Add a chart at run time.
+//            chart1.Series.Add("RuntimeSeries");
+//            chart1.Series["RuntimeSeries"].ChartType = SeriesChartType.Bar;
+//            chart1.Series["RuntimeSeries"].ChartArea = "ChartArea2";
+
+//            foreach (Usuari usuari in eager)
+//            {
+//                chart1.Series["RuntimeSeries"].Points.AddY(usuari.Comercial.Client.Count);
+//            }                        
+//        }
